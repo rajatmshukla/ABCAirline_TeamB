ec9e92dc4c144d322a6fe6e40eaa1724288650f9
Madhumithra M <madhumithra.m@ibm.com>
2021-03-15 14:37:39 +0530

    updated tests

---
+++ src/main/java/com/myapp/spring/repository/PassengerRepositoryFare.java
@@ -0,0 +1,25 @@
+package com.myapp.spring.repository;
+
+
+
+import com.myapp.spring.model.ProductFare;
+
+public interface PassengerRepositoryFare {
+
+	int calcFuelCost(ProductFare p1);
+
+	int addExpense(ProductFare p1);
+	
+	int perSeatCalc(ProductFare p1);
+	
+	int exclusiveCalc(ProductFare p1);
+	
+	int splDayCalc (ProductFare p1);
+	
+	String dispFare(String Flight_name, String classf);
+
+	ProductFare Notifyuser(String Flight_name , String classf);
+	
+
+	
+}
